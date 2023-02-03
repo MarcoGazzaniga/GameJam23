46c9bd821974091f8ddbde6fdabd682d9f042ebe
DESKTOP-EQ0E531\DeShi <samuelxbg@gmail.com>
2023-02-04 00:25:26 +0100

    Prima iterazione del playerController con movement

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    Vector2 targetPosition;
+    PlayerActionScript actions;
+
+    bool isMoving;
+    // Start is called before the first frame update
+    void Start()
+    {
+        actions = new PlayerActionScript();
+        actions.Player.Enable();
+
+        isMoving = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, Time.deltaTime * moveSpeed);
+        Vector2 currentPositionVector2 = transform.position;
+        if (!isMoving) {
+            Vector2 offset = actions.Player.Movement.ReadValue<Vector2>();
+            if (offset.x != 0 && offset.y != 0) {
+                offset.x = 0;
+            }
+            targetPosition = currentPositionVector2 + offset;
+            isMoving = true;
+        } else {
+            if (currentPositionVector2 == targetPosition) {
+                isMoving = false;
+            }
+        }
+    }
+}
